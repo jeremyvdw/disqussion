c91c5a320226138249e5635299796f5112052e16
Jérémy <jeremyvdw@gmail.com>
2011-05-19 16:06:03 +0200

    Added Disqus reports features

--- lib/disqussion/client/reports.rb
+++ lib/disqussion/client/reports.rb
@@ -1,4 +1,79 @@
 module Disqussion
   class Reports < Client
+    # Returns report on domains
+    # @accessibility: public key, secret key
+    # @methods: GET
+    # @format: json, jsonp
+    # @authenticated: false
+    # @limited: false
+    # @param options [Hash] A customizable set of options.
+    # @option options [Integer] :limit. Defaults to 25. Maximum length of 100.
+    # @option options [Integer] :page. Defaults to 0. Maximum length of 10.
+    # @option options [Array, String] :forum. Allow multiple. Defaults to null. Looks up a forum by ID (aka short name).
+    # @return [Hashie::Rash] Returns report
+    # @example Return report on comments by domains usage
+    #   Disqussion::Client.reports.domains
+    # @see: http://disqus.com/api/3.0/reports/domains.json
+    def domains(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      response = get('reports/domains', options)
+    end
+    
+    # Returns report on ips
+    # @accessibility: public key, secret key
+    # @methods: GET
+    # @format: json, jsonp
+    # @authenticated: false
+    # @limited: false
+    # @param options [Hash] A customizable set of options.
+    # @option options [Integer] :limit. Defaults to 25. Maximum length of 100.
+    # @option options [Integer] :page. Defaults to 0. Maximum length of 10.
+    # @option options [Array, String] :forum. Allow multiple. Defaults to null. Looks up a forum by ID (aka short name).
+    # @return [Hashie::Rash] Returns report
+    # @example Return report on comments by IPs usage
+    #   Disqussion::Client.reports.ips
+    # @see: http://disqus.com/api/3.0/reports/ips.json
+    def ips(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      response = get('reports/ips', options)
+    end
+    
+    # Returns report on threads
+    # @accessibility: public key, secret key
+    # @methods: GET
+    # @format: json, jsonp
+    # @authenticated: false
+    # @limited: false
+    # @param options [Hash] A customizable set of options.
+    # @option options [Integer] :limit. Defaults to 25. Maximum length of 100.
+    # @option options [Integer] :page. Defaults to 0. Maximum length of 10.
+    # @option options [Array, String] :forum. Allow multiple. Defaults to null. Looks up a forum by ID (aka short name).
+    # @return [Hashie::Rash] Returns report
+    # @example Return report on comments by threads
+    #   Disqussion::Client.reports.threads
+    # @see: http://disqus.com/api/3.0/reports/threads.json
+    def threads(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      response = get('reports/threads', options)
+    end
+    
+    # Returns report on users
+    # @accessibility: public key, secret key
+    # @methods: GET
+    # @format: json, jsonp
+    # @authenticated: false
+    # @limited: false
+    # @param options [Hash] A customizable set of options.
+    # @option options [Integer] :limit. Defaults to 25. Maximum length of 100.
+    # @option options [Integer] :page. Defaults to 0. Maximum length of 10.
+    # @option options [Array, String] :forum. Allow multiple. Defaults to null. Looks up a forum by ID (aka short name).
+    # @return [Hashie::Rash] Returns report
+    # @example Return report on comments by users
+    #   Disqussion::Client.reports.users
+    # @see: http://disqus.com/api/3.0/reports/users.json
+    def users(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      response = get('reports/users', options)
+    end
   end
 end
---
+++ spec/disqussion/client/reports_spec.rb
@@ -0,0 +1,63 @@
+require 'helper'
+
+describe Disqussion::Reports do
+  Disqussion::Configuration::VALID_FORMATS.each do |format|
+    context ".new(:format => '#{format}')" do
+      before do
+        @client = Disqussion::Client.reports
+      end
+      
+      describe ".domains" do
+        before do
+          stub_get("reports/domains.json", :query => { }).
+            to_return(:body => fixture("reports/domains.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+        
+        it "returns report on domains." do
+          @client.domains
+          a_get("reports/domains.json", :query => { }).
+            should have_been_made
+        end
+      end
+      
+      describe ".ips" do
+        before do
+          stub_get("reports/ips.json", :query => { }).
+            to_return(:body => fixture("reports/ips.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+        
+        it "returns report on ips." do
+          @client.ips
+          a_get("reports/ips.json", :query => { }).
+            should have_been_made
+        end
+      end
+      
+      describe ".threads" do
+        before do
+          stub_get("reports/threads.json", :query => { }).
+            to_return(:body => fixture("reports/threads.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+        
+        it "returns report on threads." do
+          @client.threads
+          a_get("reports/threads.json", :query => { }).
+            should have_been_made
+        end
+      end
+      
+      describe ".users" do
+        before do
+          stub_get("reports/users.json", :query => { }).
+            to_return(:body => fixture("reports/users.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+        
+        it "returns report on users." do
+          @client.users
+          a_get("reports/users.json", :query => { }).
+            should have_been_made
+        end
+      end
+    end
+  end
+end
