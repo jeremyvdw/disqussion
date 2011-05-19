66e519e68a8355ca5af3ef8bffcac52541fc3f4f
Jérémy <jeremyvdw@gmail.com>
2011-05-19 16:47:08 +0200

    Added Disqus Exports feature

--- lib/disqussion/client/exports.rb
+++ lib/disqussion/client/exports.rb
@@ -1,4 +1,26 @@
 module Disqussion
   class Exports < Client
+    # Export a forum
+    # @accessibility: public key, secret key
+    # @methods: POST
+    # @format: json, jsonp
+    # @authenticated: true
+    # @limited: false
+    # @param forum [String] Forum short name (aka forum id).
+    # @return [Hashie::Rash] Export infos
+    # @param options [Hash] A customizable set of options.
+    # @option options [String] :format. Defaults to "xml". Choices: xml, xml-old
+    # @example Export forum "the88"
+    #   Disqussion::Client.exports.exportForum("the88")
+    # @see: http://disqus.com/api/3.0/exports/exportForum.json
+    def exportForum(*args)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      if args.size == 1
+        options.merge!(:forum => args[0])
+        response = post('exports/exportForum', options)
+      else
+        puts "#{Kernel.caller.first}: exports.exportForum expects an arguments: forum"
+      end
+    end
   end
 end
---
+++ spec/disqussion/client/export_spec.rb
@@ -0,0 +1,24 @@
+require 'helper'
+
+describe Disqussion::Exports do
+  Disqussion::Configuration::VALID_FORMATS.each do |format|
+    context ".new(:format => '#{format}')" do
+      before do
+        @client = Disqussion::Client.exports
+      end
+      
+      describe ".create" do
+        before do
+          stub_post("exports/exportForum.json", :body => { :forum => "the88" }).
+            to_return(:body => fixture("exports/exportForum.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+        end
+        
+        it "exports a forum." do
+          @client.exportForum("the88")
+          a_post("exports/exportForum.json", :body => { :forum => "the88" }).
+            should have_been_made
+        end
+      end
+    end
+  end
+end
